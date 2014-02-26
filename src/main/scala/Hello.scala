8bd1e6f02053daaab43238d1819ef38ea06ebd12
Anton Kumaigorodskiy <anton.kumaigorodskiy@outlook.com>
2014-02-26 23:53:51 +0200

    Update Hello.scala

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -3,20 +3,19 @@ import scala.language.postfixOps
 import SharedSecret._
 
 object Hello {
-  def main( args: Array[String] ): Unit =
-    Try {
-      // 4 participants in total. Any 3 of them can unlock a secret.
-      val secretGenerator = SharedSecret(numPeople = 3, ofHowMany = 4)
-      val (shares, genesisMat) = secretGenerator.create("There is no spoon")
+  def main( args: Array[String] ): Unit = Try {
+    // 4 participants in total. Any 3 of them can unlock a secret.
+    val secretGenerator = SharedSecret(numPeople = 3, ofHowMany = 4)
+    val (shares, genesisMat) = secretGenerator.create("There is no spoon")
     
-      // In this case <shares> is a list of 4 keys and corresponding generator vectors. 
-      // We give one of these keys to each of 4 participants. We can combine any 3 keys to obtain a secret. 
-      // Less than 3 keys won't do. Brute force selection isn't feasible due to extremely large number of possible variants.
-      val random3of4 = util.Random.shuffle(shares.toList combinations secretGenerator.numPeople) next;
-      SharedSecret.decode(genesisMat, random3of4:_*)
-    } match {
+    // In this case <shares> is a list of 4 keys and corresponding generator vectors. 
+    // We give one of these keys to each of 4 participants. We can combine any 3 keys to obtain a secret. 
+    // Less than 3 keys won't do. Brute force selection isn't feasible due to extremely large number of possible variants.
+    val random3of4 = util.Random.shuffle(shares.toList combinations secretGenerator.numPeople) next;
+    SharedSecret.decode(genesisMat, random3of4:_*)
+  } match {
       // Result is: There is no spoon
-      case Success(s) => println(s"Result is: ${bits2Chars(s).mkString}")
-      case Failure(e) => println(s"No luck this time: $e")
-    }
+    case Success(s) => println(s"Result is: ${bits2Chars(s).mkString}")
+    case Failure(e) => println(s"No luck this time: $e")
+  }
 }
